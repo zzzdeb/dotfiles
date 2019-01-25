6fe40baa8aeb82208ec9f1fde3171fe3a046e318
Zolboo <zolboo.deb@gmail.com>
2019-01-25 03:56:48 +0100

    polybar

---
+++ polybar/polybar_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt install python-xcbgen
+sudo apt install libjsoncpp-dev
+sudo apt install libmpdclient-dev \
+libcairo2-dev \
+libxcb1-dev \
+libxcb-util0-dev \
+libxcb-randr0-dev \
+libxcb-composite0-dev \
+xcb-proto \
+libxcb-image0-dev \
+libxcb-ewmh-dev  \
+libxcb-icccm4-dev \
+build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+
+sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev i3-wm libmpdclient-dev libcurl4-openssl-dev libiw-dev libnl-genl-3-dev
