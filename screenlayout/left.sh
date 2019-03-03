3cddf983bec6d0ccd21ca208851268ad143e9191
Zolboo <zolboo.deb@gmail.com>
2019-03-03 16:52:41 +0100

    minor

--- screenlayout/left.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI2 --off --output HDMI1 --primary --mode 1920x1080 --pos 1368x0 --rotate normal --output DP1 --off --output eDP1 --mode 1360x768 --pos 0x312 --rotate normal --output VIRTUAL1 --off
