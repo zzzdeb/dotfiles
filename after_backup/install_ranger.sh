8c8dd077e0bcf39c5accc322f19076948cbc2dad
Zolboo <zolboo.deb@gmail.com>
2019-03-08 22:58:14 +0100

    afterbackup organization

--- after_backup/install_ranger.sh
+++ after_backup/install_ranger.sh
@@ -4,8 +4,7 @@
 pip install ranger
 sudo apt install highlight
 
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf"
-https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 cd /tmp
 git clone https://github.com/alexanderjeurissen/ranger_devicons
