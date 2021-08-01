a008220ce7516c12d8a106553094868fd4ea0d46
Zolboo <zolboo.deb@gmail.com>
2021-08-01 15:44:07 +0200

    Update

--- scripts/i3cmds/i3fullscreenDirections.py
+++ scripts/i3cmds/i3fullscreenDirections.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     i3 = Connection()
     last_focused = i3.get_tree().find_focused()
     if not last_focused.fullscreen_mode:
-        i3.command('focus '+direction)
-        sys.exit()
-
-    i3.command('fullscreen; focus '+direction)
-    cur_focused = i3.get_tree().find_focused()
-    if last_focused.workspace().id == cur_focused.workspace().id:
-        i3.command('fullscreen')
+        last_focused.command('focus '+direction)
         sys.exit()
 
     last_focused.command('fullscreen')
+    last_focused.command('focus '+direction)
+    cur_focused = i3.get_tree().find_focused()
+    if last_focused.workspace().id == cur_focused.workspace().id:
+        cur_focused.command('fullscreen')
+    else:
+        last_focused.command('focus; fullscreen')
+    sys.exit()
