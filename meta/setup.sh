e2609b260ef851006f5b5d74739b68748238383e
Zolboo Erdenebayar <zolboo.erdenebayar@indurad.com>
2025-11-05 19:01:19 +0100

    setup script

---
+++ meta/setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt install python3-virtualenvwrapper ranger python3-pip sxhkd mutt urlview i3 i3blocks arandr rofi pcmanfm shutter pavucontrol feh xfce4-power-manager bat fonts-font-awesome fonts-dejavu fonts-noto pipx npm keychain
